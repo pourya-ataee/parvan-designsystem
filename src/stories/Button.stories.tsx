05e5dc2eb89d5d5a6e52de11092664efc440fef1
Pourya <p.ataee.dev@gmail.com>
2023-04-03 17:35:17 +0430

    create button component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from "../components/Button";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Button } from '../components';
 
 export default {
   title: 'Button',
   component: Button,
+  argTypes: {
+    variant: {
+      options: ['filled', 'outlined', 'text', 'elevated', 'tonal'],
+      control: 'select'
+    },
+    theme: {
+      control: 'none'
+    },
+    as: {
+      control: 'none'
+    },
+    forwardedAs: {
+      control: 'none'
+    }
+  }
+} as ComponentMeta<typeof Button>;
+
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Filled = Template.bind({});
+Filled.args = {
+  variant: 'filled',
+  rounded: true,
+  isFullyRounded: true,
+  children: 'submit',
+  disabled: false,
 };
 
-export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>;
-export const Primary = () => <Button primary onClick={action('clicked')}>Primary Button</Button>;
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: 'outlined',
+  rounded: true,
+  isFullyRounded: true,
+  children: 'submit',
+  disabled: false,
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  variant: 'text',
+  rounded: true,
+  isFullyRounded: true,
+  children: 'submit',
+  disabled: false,
+};
+
+export const Elevated = Template.bind({});
+Elevated.args = {
+  variant: 'elevated',
+  rounded: true,
+  isFullyRounded: true,
+  children: 'submit',
+  disabled: false,
+};
+
+export const Tonal = Template.bind({});
+Tonal.args = {
+  variant: 'tonal',
+  rounded: true,
+  isFullyRounded: true,
+  children: 'submit',
+  disabled: false,
+};
+
+// export const Default = () => <Button />;
+// export const Primary = () => <Button filled rounded onClick={action('clicked')}>Primary Button</Button>;
