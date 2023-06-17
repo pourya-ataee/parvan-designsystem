a72b54034e8ae234b2c2eae51c81ac6113acc6f3
pourya-ataee <p.ataee.dev@gmail.com>
2023-06-17 13:28:34 +0330

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import babel from '@rollup/plugin-babel';
 
 export default defineConfig({
   plugins: [react(), dts()],
@@ -13,16 +14,22 @@ export default defineConfig({
       formats: ['cjs'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components', 'clsx'],
+      plugins: [
+        babel({
+          exclude: 'node_modules/**',
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          babelHelpers: 'bundled',
+          plugins: [['styled-components', { ssr: true }]],
+        }),
+      ],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           'react': 'React',
-          'react-dom': 'ReactDOM',
-          'styled-components': 'styled',
-          'clsx': 'clsx'
+          'react-dom': 'ReactDOM'
         },
         exports: 'named',
-      },
+      }
     },
   },
 });
