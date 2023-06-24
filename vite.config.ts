1a5b959e98f182267b9a8f5cda97934477db639f
pourya-ataee <p.ataee.dev@gmail.com>
2023-06-24 12:27:46 +0330

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'parvan-designsystem',
-      fileName: (format) => format === 'cjs' ? `index.${format}` : `index.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
       formats: ['cjs', 'es'],
     },
     rollupOptions: {
